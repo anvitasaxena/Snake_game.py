b4bb81741a4a0951b54175611b313f7caf93de8d
anvitasaxena <51175626+anvitasaxena@users.noreply.github.com>
2020-05-19 10:43:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from PIL import Image,ImageTk
+
+MOVE_INCREMENT=20
+MOVES_PER_SECOND= 15
+GAME_SPEED=1000//MOVES_PER_SECOND
+
+
+root= tk.Tk()
+root.title("Snake")
+root.resizable(False,False)
+
+class Snake(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600,height=620,background="black", highlightthickness=0)
+        
+        self.snake_positions=[(100,100), (80,100),(60,100)]
+        self.food_position=(200,100)
+        self.score=0
+        
+        self.load_assets()
+        self.create_objects()
+        
+        self.after(75, self.perform_actions)
+        
+        
+    def load_assets(self):
+        try:
+            self.snake_body_image= Image.open("snake.png")
+            self.snake_body = ImageTk.PhotoImage(self.snake_body_image)
+            
+            self.food_image= Image.open("food.png")
+            self.food= ImageTk.PhotoImage(self.food_image)
+            
+        except IOError as error:
+            print(error)
+            root.destroy()
+            
+    def create_objects(self):
+        
+        self.create_text(
+         45,12, text = f"Score: {self.score}",tags="score",fill="#fff" , font=("TkDefaultFont",14)
+            
+    
+        )
+        
+        
+        
+        for x_position,y_position in self.snake_positions:
+            self.create_image(x_position, y_position, image=self.snake_body, tag="snake")
+         
+        self.create_image(self.food_position[0], self.food_position[1] , image=self.food, tag = "food")  
+        self.create_rectangle(7,27,593,613, outline="#525d69")
+        
+    def move_snake(self):
+        head_x_position, head_y_position = self.snake_positions[0]
+        new_head_position = (head_x_position + MOVE_INCREMENT, head_y_position)
+        
+        self.snake_positions=[new_head_position]+ self.snake_positions[:-1]
+        
+        for segment, position in zip(self.find_withtag("snake"), self.snake_positions):
+            self.coords(segment, position)
+        
+    def perform_actions(self):
+        if self.check_collisions():
+            return
+        
+        self.move_snake()
+        self.after(GAME_SPEED, self.perform_actions)
+        
+    def check_collisions(self):
+        head_x_position, head_y_position = self.snake_positions[0]
+        
+        return(
+        head_x_position in (0,600)
+        or head_y_position in (20,620)
+        or (head_x_position, head_y_position) in self.snake_positions[1:]   
+        
+        
+        )
+        
+        
+        
+        
+        
+board = Snake()        
+board.pack()
+root.mainloop()
